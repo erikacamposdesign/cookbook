eda9ed4b4caf921a7ce1a2aa1745ddad6c3802b0
erikacamposdesign <erika.campos.design@gmail.com>
2018-07-04 01:27:40 -0300

    [WIP] 6 Visitante filtra receitas por cozinha

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,4 +5,16 @@ def show
   @recipe = Recipe.find(id)
 end
 
+def new
+  @recipe = Recipe.new()
+end
+
+def create
+  recipe_params = params.require(:recipe).permit(:title, :recipe_type, :cuisine_id,
+    :difficulty, :cook_time, :ingredients, :method)
+  @recipe = Recipe.new(recipe_params)
+  @recipe.save
+  redirect_to recipe_path(@recipe.id)
+end
+
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,4 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  end
+end
